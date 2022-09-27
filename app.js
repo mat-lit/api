22c0009142ae2d063301ff97340e1163a29ed27b
Mathis Adler <contact@mathis-adler.dev>
2022-09-27 17:15:34 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path');
+
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+  res.sendFile('public/index.html', { root: '.' })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
