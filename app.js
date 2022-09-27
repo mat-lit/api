b2567868f3b59fa14575ee72870f4b95341cb60c
Mathis Adler <contact@mathis-adler.dev>
2022-09-27 18:07:22 +0200

    new

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.sendFile('public/index.html', { root: '.' })
+  res.sendFile('public/index.js', { root: '.' })
+})
+
+app.get('/api', (req, res) => {
+  res.send('nice one!')
 })
 
 app.listen(port, () => {
