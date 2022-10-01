ecb1efbd7382d88a0902c68bb947a656bb97d125
Mathis Adler <contact@mathis-adler.dev>
2022-10-01 09:18:42 +0200

    deleted index.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.sendFile('public/index.html', { root: '.' })
-  res.sendFile('public/index.js', { root: '.' })
 })
 
 app.get('/api', (req, res) => {
