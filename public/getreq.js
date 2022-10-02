e1a463708f198dc0e2a7b92671a9356ab584881c
Mathis Adler <contact@mathis-adler.dev>
2022-10-02 15:03:40 +0200

    new page

--- public/getreq.js
+++ public/getreq.js
@@ -12,3 +12,21 @@ $.ajax({
       console.log(xhr)
     }
   });
+
+
+
+
+  $.ajax({
+    url: "http://api.wossidia.de/isebelindexids",
+    type: "get", 
+    data: { 
+      word: "moort",
+    },
+    dataType: "json",
+    success: function(response) {
+      console.log(response)
+    },
+    error: function(xhr) {
+      console.log(xhr)
+    }
+  });
